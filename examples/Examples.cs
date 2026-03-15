69a5e00a859e2ce834a7adaae403e31d5bb9aff6
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2026-03-16 01:38:56 +0800

    Move examples fro src to root

---
+++ examples/Examples.cs
@@ -0,0 +1,19 @@
+// Copyright (c) 2026 TirsvadWeb. All rights reserved. 
+//  No warranty, explicit or implicit, provided.
+
+using TirsvadCLI.AnsiCode;
+
+using static TirsvadCLI.AnsiCode.AnsiConstants;
+
+namespace Example;
+
+internal class Examples
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(AnsiCode.Colorize("Hello, World!", BLUE));
+        Console.WriteLine(AnsiCode.Colorize("Hello, World!", BRIGHT_YELLOW, BG_BRIGHT_BLUE));
+        Console.WriteLine(AnsiCode.Colorize("Error message!", RED));
+        Console.WriteLine(AnsiCode.Colorize("Error message!", BLACK, BG_RED));
+    }
+}
